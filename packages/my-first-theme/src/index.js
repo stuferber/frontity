bbd00bbed391423c87f1574125be077e4e7cf565
Stuart Ferber <stuart.ferber@gmail.com>
2022-10-28 02:20:45 +0200

    Created own theme

---
+++ packages/my-first-theme/src/index.js
@@ -0,0 +1,30 @@
+// File: /packages/my-first-theme/src/index.js
+
+import Root from "./components"
+import link from "@frontity/html2react/processors/link";
+
+const myFirstTheme = {
+  name: "my-first-theme",
+  roots: {
+    theme: Root,
+  },
+  state: {
+    theme: {
+      isUrlVisible: false,
+    },
+  },
+  actions: {
+    theme: {
+      toggleUrl: ({ state }) => {
+        state.theme.isUrlVisible = !state.theme.isUrlVisible
+      },
+    },
+  },
+  libraries: {
+    html2react: {
+      processors: [link]
+    }
+  }
+}
+
+export default myFirstTheme
